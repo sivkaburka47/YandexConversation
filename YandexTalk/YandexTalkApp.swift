a56ab7a3b4ac9c6dc2005bf345978c28da4c89c3
BogdanTarchenko <tarchenko.bogdan@outlook.com>
2025-06-01 10:15:45 +0300

    Initial Commit

---
+++ YandexTalk/YandexTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexTalkApp.swift
+//  YandexTalk
+//
+//  Created by Богдан Тарченко on 01.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct YandexTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
